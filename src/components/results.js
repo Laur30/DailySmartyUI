44b3211ca23a86817c3d13c4f8c1564477ed5723
Laur30 <laurwints@gmail.com>
2020-06-14 10:38:42 -0500

    setup results component

---
+++ src/components/results.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Logo from './logo';
+
+class Results extends Component {
+    render() {
+        return (
+            <div>
+                <Logo/>
+            </div>
+        )
+    } 
+}
+export default Results;
