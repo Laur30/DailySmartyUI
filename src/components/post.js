a12e19d4a0bf0b13d4780a3d7a2d6c209f59e47b
Laur30 <laurwints@gmail.com>
2020-06-17 08:45:30 -0500

    finished JS substrings for post links

--- src/components/post.js
+++ src/components/post.js
@@ -18,14 +18,34 @@ class Post extends Component {
         return topics;
     }
 
+    getNameForPostLink(str) {
+        var n = str.lastIndexOf('/');
+        var link = str.substring(n + 1, str.length);
+
+        if((n+1) == str.length) {
+            link = str.slice(0, n);
+            n = link.lastIndexOf('/');
+            link = str.substring(n + 1, str.length - 1);
+        }    
+
+        if(link.includes('.html')) {
+            link = link.substring(0, link.length - 5);
+        }
+        if(link.includes('.htm')) {
+            link = link.substring(0, link.length - 4);
+        }
+
+        return link;
+    }
+
     renderLinks() {
         let links = this.props.post_links.map((post_link, index) => {
             return (
                 <div className="post-link" key={index}>
                     <div className="post-link__box"></div>
                     <div className="post-link__link">
-                        <a>{post_link.link_url}</a>
-                </div>
+                        <a href={post_link.link_url}>{this.getNameForPostLink(post_link.link_url)}</a>
+                    </div>
                 </div>
             )
         })
@@ -37,7 +57,7 @@ class Post extends Component {
             return (
                 <li className="recent-post">
                     <div className="recent-post__title">
-                        {this.props.title}
+                        <a href={this.props.url_for_post}>{this.props.title}</a>
                     </div>
                     <div className="recent-post__topics">
                         {this.renderTopics()}
@@ -62,17 +82,14 @@ class Post extends Component {
                         duration={500}
                         height={this.state.height}
                     >
-                       <div className="result-post__links">
-                        {this.renderLinks()}
-                       </div>
+                        <div className="result-post__links">
+                            {this.renderLinks()}
+                        </div>
                     </AnimateHeight>
-
-                    <div className="result-post__links">
-                        {this.renderLinks()}
-                    </div>
                 </li>
             )
         }
+
     }
 }
 
